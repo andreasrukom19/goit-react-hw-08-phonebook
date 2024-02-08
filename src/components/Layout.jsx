87233a54a919b52faaf97931820d4b3d76da556b
Андрій <rukom@ukr.net>
2024-02-08 14:24:54 +0200

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,13 @@
 import React, { Suspense } from 'react';
 import { AppBar } from './AppBar/AppBar';
 import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectIsLoading } from '../redux/auth/selectors';
 import { Loader } from './Loader/Loader';
 
 export const Layout = () => {
-  const isLoading = useSelector(selectIsLoading);
-
   return (
     <div className="wrapper">
       <AppBar />
-      <Suspense fallback={isLoading ? <Loader /> : ''}>
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
     </div>
